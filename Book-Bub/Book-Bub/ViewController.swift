efaca51e31978d5c75959ba7cbeb45fc245a7c58
AfafSaleh <Afaf.Saleh.med.s@tuwaiq.edu.sa>
2021-12-09 11:05:31 +0300

    ..... Create Func getImage() < For show images books. @AfafSaleh55 @SHURUQ10

--- Book-Bub/Book-Bub/ViewController.swift
+++ Book-Bub/Book-Bub/ViewController.swift
@@ -7,45 +7,99 @@
 
 import UIKit
 
+
+
 struct Response : Codable {
-  var kind: String
-  var totalItems: Int
-  var items: [VolumeInfo]
+    var kind: String
+    var totalItems: Int
+    var items: [VolumeInfo]
 }
 //volumeInfo
 
 struct VolumeInfo: Codable {
-  var volumeInfo: Book
+    var volumeInfo: Book
 }
 
 struct Book: Codable {
-  var title: String
-  var authors: [String]
+    var title: String
+    var authors: [String]
+    var imageLinks: ImageLinks
 }
 
-class ViewController : UIViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    getBook()
-  }
-  func getBook(){
-    // https://www.googleapis.com/books/v1/volumes?q=swift
-    var bookurl = URLComponents()
-    bookurl.scheme = "https"
-    bookurl.host = "www.googleapis.com"
-    bookurl.path = "/books/v1/volumes"
-    bookurl.query = "q=swift"
+struct ImageLinks: Codable {
+    var smallThumbnail: String
+    var thumbnail: String
+}
 
-    let urlbookRequest = URLRequest(url: bookurl.url!)
-    let task = URLSession.shared.dataTask(with: urlbookRequest) { (data: Data?, response: URLResponse?, error: Error?) in
-      do {
-        let jsomDecoder = JSONDecoder()
-        let result = try jsomDecoder.decode(Response.self, from: data!)
-        print (result)
-      } catch {
-        print(error)
-      }
+
+class ViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arrImages.count
     }
-    task.resume()
-  }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BOOKID") as! BookCell
+        cell.bookImage.image = arrImages[indexPath.row]
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    var arrImages = [UIImage]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getBook()
+    }
+    func getBook(){
+        // https://www.googleapis.com/books/v1/volumes?q=swift
+        var bookurl = URLComponents()
+        bookurl.scheme = "https"
+        bookurl.host = "www.googleapis.com"
+        bookurl.path = "/books/v1/volumes"
+        bookurl.query = "q=harry+potter"
+        
+        let urlbookRequest = URLRequest(url: bookurl.url!)
+        let task = URLSession.shared.dataTask(with: urlbookRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            do {
+                if let data = data {
+                    let jsomDecoder = JSONDecoder()
+                    let result = try jsomDecoder.decode(Response.self, from: data)
+                    for item in result.items {
+                        self.getImage(item.volumeInfo.imageLinks.thumbnail)
+                    }
+                    print (result)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    func getImage(_ urlString: String) {
+        
+        let url = URL(string: urlString)!
+        let task = URLSession.shared.dataTask(with: url) { data, _, erroe in
+            guard let data = data , erroe == nil else {
+                return
+            }
+            let image = UIImage(data: data)
+            self.arrImages.append(image!)
+            DispatchQueue.main.async {
+                self.tableView?.reloadData()
+            }
+            
+        }
+        task.resume()
+    }
+    
+}
+
+class BookCell: UITableViewCell {
+    
+    @IBOutlet weak var bookImage: UIImageView!
+    
 }
